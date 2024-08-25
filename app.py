2fb57d7eef1b010b44a873f03329628c12bddac2
Gandharv Kulkarni <114131432+gandharvk422@users.noreply.github.com>
2024-08-26 03:23:27 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+st.set_page_config(page_title="Salarize Yourself - Predict Your Own Salary", page_icon="💸")
+
 dataset = pd.read_csv("Salary_Data.csv")
 X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
